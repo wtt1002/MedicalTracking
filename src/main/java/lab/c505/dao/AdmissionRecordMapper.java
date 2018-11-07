7a934cd3b1689772d9ddd3eb869bcfeb8ead2f8e
wtt1002 <wtt1002@qq.com>
2018-11-07 14:59:24 +0800

    mybatis generator dao/mapper

--- src/main/java/lab/c505/dao/AdmissionRecordMapper.java
+++ src/main/java/lab/c505/dao/AdmissionRecordMapper.java
@@ -1,15 +1,28 @@
 package lab.c505.dao;
 
+import java.util.List;
 import lab.c505.entity.AdmissionRecord;
+import lab.c505.entity.AdmissionRecordExample;
+import org.apache.ibatis.annotations.Param;
 
 public interface AdmissionRecordMapper {
+    int countByExample(AdmissionRecordExample example);
+
+    int deleteByExample(AdmissionRecordExample example);
+
     int deleteByPrimaryKey(Long ar_id);
 
     int insert(AdmissionRecord record);
 
     int insertSelective(AdmissionRecord record);
 
+    List<AdmissionRecord> selectByExample(AdmissionRecordExample example);
+
     AdmissionRecord selectByPrimaryKey(Long ar_id);
+
+    int updateByExampleSelective(@Param("record") AdmissionRecord record, @Param("example") AdmissionRecordExample example);
+
+    int updateByExample(@Param("record") AdmissionRecord record, @Param("example") AdmissionRecordExample example);
 
     int updateByPrimaryKeySelective(AdmissionRecord record);
 
