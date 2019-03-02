c52368582eef9b435e72a34f3e293fb502eb198d
wtt1002 <wtt1002@qq.com>
2019-03-02 22:07:21 +0800

    清空测试类

---
+++ src/test/java/lab/c505/service/impl/AccountServiceImplTest.java
@@ -0,0 +1,23 @@
+package lab.c505.service.impl;
+
+import lab.c505.BaseTest;
+import org.junit.Test;
+import lab.c505.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Package: lab.c505.service.impl
+ * Description： TODO
+ * Author: TingTing W
+ * Date: Created in 2019/2/19 19:41
+ */
+public class AccountServiceImplTest extends BaseTest {
+//
+//    @Autowired
+//    AccountService accountService;
+//
+//    @Test
+//    public void testLogin(){
+//    System.out.println(accountService.checkLogin("131632611370","123456"));
+//    }
+}
---
+++ src/test/java/lab/c505/service/impl/MedicalHistoryServiceTest.java
@@ -0,0 +1,22 @@
+package lab.c505.service.impl;
+
+import lab.c505.BaseTest;
+import lab.c505.service.MedicalHistoryService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Package: lab.c505.service.impl
+ * Description： TODO
+ * Author: TingTing W
+ * Date: Created in 2019/2/27 14:28
+ */
+public class MedicalHistoryServiceTest extends BaseTest {
+//    @Autowired
+//    MedicalHistoryService medicalHistoryService;
+//    @Test
+//    public void testGetMainDiagnose(){
+//       String diagnose =  medicalHistoryService.getLastMainDiagnose(1);
+//        System.out.println("什么？"+diagnose);
+//    }
+}
