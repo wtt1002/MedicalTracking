dcb4b54a87c4cebb9d4a03de013f61b5cb896baf
wtt1002 <wtt1002@qq.com>
2019-03-02 21:41:24 +0800

    异常类

---
+++ src/main/java/lab/c505/exeception/VersionException.java
@@ -0,0 +1,25 @@
+package lab.c505.exeception;
+/**
+ * 
+ *@comment  
+
+ *@author huchunhe
+
+ *@date 2018-10-10
+
+ *@version 1.0.0
+
+ *@company 东巴文信息技术有限公司
+ */
+public class VersionException extends RuntimeException {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public VersionException(String arg0) {
+        super(arg0);
+    }
+
+}
