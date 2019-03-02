a72f26aa3ca7f378cf0192ffaa464d8273bff8d1
wtt1002 <wtt1002@qq.com>
2019-03-02 17:51:08 +0800

    患者dto

---
+++ src/main/java/lab/c505/dto/PatientDto.java
@@ -0,0 +1,36 @@
+package lab.c505.dto;
+
+import lab.c505.entity.Patient;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.time.LocalDate;
+
+/**
+ * Package: lab.c505.dto
+ * Description： 添加患者dto
+ * Author: TingTing W
+ * Date: Created in 2019/2/28 15:55
+ */
+
+@Data
+@Accessors(chain = true)
+public class PatientDto {
+    private Patient patient;
+    private String birthdayStr;
+
+    public PatientDto setBirthday() throws Exception{
+        if (birthdayStr != null && !birthdayStr.equals("")  && patient != null){
+            patient.setBirthday(LocalDate.parse(birthdayStr));
+        }else {
+            patient.setBirthday(null);
+        }
+        return this;
+    }
+
+    public void getBirthday() throws Exception{
+        if (patient != null && patient.getBirthday() != null){
+            this.birthdayStr = patient.getBirthday().toString();
+        }
+    }
+}
