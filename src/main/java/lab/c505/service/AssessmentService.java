1389a8923efa449ac5d105aecfbd1e6c48232d24
wtt1002 <wtt1002@qq.com>
2019-05-03 21:03:24 +0800

    添加病历，入院评估

--- src/main/java/lab/c505/service/AssessmentService.java
+++ src/main/java/lab/c505/service/AssessmentService.java
@@ -3,6 +3,8 @@ package lab.c505.service;
 import lab.c505.entity.Assessment;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * <p>
  * 入院评估表 服务类
@@ -12,5 +14,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2019-02-19
  */
 public interface AssessmentService extends IService<Assessment> {
+
+    List<Assessment> getOneAssessment(String medicalHistoryId);
+    String addOneAssessment(Assessment assessment) throws Exception;
+    Assessment updateOneAssessment(Assessment assessment) throws Exception;
 
 }
