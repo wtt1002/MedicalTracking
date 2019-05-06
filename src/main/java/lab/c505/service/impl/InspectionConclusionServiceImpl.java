0c1965dd75d047b5915856fe9e725cb40330ed47
wtt1002 <wtt1002@qq.com>
2019-05-06 14:57:55 +0800

    入院康复检查

--- src/main/java/lab/c505/service/impl/InspectionConclusionServiceImpl.java
+++ src/main/java/lab/c505/service/impl/InspectionConclusionServiceImpl.java
@@ -1,9 +1,11 @@
 package lab.c505.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lab.c505.entity.InspectionConclusion;
 import lab.c505.mapper.InspectionConclusionMapper;
 import lab.c505.service.InspectionConclusionService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +19,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class InspectionConclusionServiceImpl extends ServiceImpl<InspectionConclusionMapper, InspectionConclusion> implements InspectionConclusionService {
 
+    @Autowired
+    InspectionConclusionMapper inspectionConclusionMapper;
+
+    @Override
+    public InspectionConclusion getOneConclusion(String medicalHistoryId, int examIndex, String examCategory) {
+
+        QueryWrapper<InspectionConclusion> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq(InspectionConclusion.MEDICAL_HISTORY_ID,medicalHistoryId)
+                .eq(InspectionConclusion.EXAM_INDEX,examIndex)
+                .eq(InspectionConclusion.EXAM_CATEGORY,examCategory);
+        return inspectionConclusionMapper.selectOne(queryWrapper);
+    }
 }
