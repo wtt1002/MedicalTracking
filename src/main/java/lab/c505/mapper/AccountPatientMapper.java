741ab551907ea874c7cbe46995c370e98cc8a942
fle <1509182803@qq.com>
2019-09-29 15:44:33 +0800

    修复患者列表like查询bug

--- src/main/java/lab/c505/mapper/AccountPatientMapper.java
+++ src/main/java/lab/c505/mapper/AccountPatientMapper.java
@@ -21,7 +21,7 @@ public interface AccountPatientMapper extends BaseMapper<AccountPatient> {
             "WHERE t_account_patient.account_id = #{account_id} and t_patient.delete_flag != 1 " +
             "and t_account_patient.patient_id = t_patient.patient_id " +
             "<if test='filter!=null'>" +
-            "and name like '%'#{filter}'%'" +
+            "and name like CONCAT('%',#{filter},'%')" +
             "</if>" +
             " ORDER BY patient_id</script>")
     List<Patient> getPatientByAccountId(Page page, @Param("account_id") String accountId, @Param("filter") String filter);
